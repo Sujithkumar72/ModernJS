97a9259767e7c2836c08e4d69a9b7f130e6ddc04
Sujith kumar <k.sujithkumar@outlook.com>
2021-03-09 12:50:01 +0530

    Array learning completed

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
-const name ="John";
-const age = 30;
-const job ="Web Developer";
-const city = "Chidambaram";
+const person={
+    firstName: "Steve",
 
-//without Template strings
+}
 
-let html;
+let val;
 
-// html = "<ul><li>Name: "+ name + "</li><li>Age: "+age+"</li><li>Job: "+ job+"</li><li>City: "+city+"</li></ul>";
-// console.log(html);
 
-//with template literals
-html = `<h2><ul>
-        <li>Name: ${name}</li>
-        <li>Age: ${age}</li>
-        <li>Job: ${job}</li>
-        <li>City: ${city}</li>
-        <li>${age>30 ? "Age is not above 30": "Above 30"}</li>
-    </ul>
-</h2>`;
+// // const name ="John";
+// // const age = 30;
+// // const job ="Web Developer";
+// // const city = "Chidambaram";
 
-document.body.innerHTML  = html;
+// // //without Template strings
+
+// // let html;
+
+// // // html = "<ul><li>Name: "+ name + "</li><li>Age: "+age+"</li><li>Job: "+ job+"</li><li>City: "+city+"</li></ul>";
+// // // console.log(html);
+
+// // //with template literals
+// // html = `<h2><ul>
+// //         <li>Name: ${name}</li>
+// //         <li>Age: ${age}</li>
+// //         <li>Job: ${job}</li>
+// //         <li>City: ${city}</li>
+// //         <li>${age>30 ? "Age is not above 30": "Above 30"}</li>
+// //     </ul>
+// // </h2>`;
+
+// // document.body.innerHTML  = html;
+
+
+// //array of numbers
+// const numbers=[43,54, 87, 45, 56, 67];
+// const numbers2 = new Array(12, 23, 34,445);
+
+// document.body.innerHTML= numbers2;
+// const fruit =["banana","orange","grapes"];
+// const mixed =[22,"Hello", true, undefined, null, {a:1, b:2}, new Date()];
+// let val;
+
+// val = mixed[5];
+// // console.log(Array.isArray(mixed));
+// console.log(val);
+
+
+// val2 = numbers.indexOf(56);
+// console.log(val2);
+
+// numbers.push(100);
+// numbers.unshift(120);
+
+// val3 =numbers.pop();
+// numbers.shift();
+
+// console.log(numbers);
+// console.log(val3);
+// //splice values
+
+// numbers.splice(2,2);
+// console.log(numbers);
+
+// val4= numbers.concat(fruit);
+
+
+// console.log(val4);
+// console.log(fruit.sort());

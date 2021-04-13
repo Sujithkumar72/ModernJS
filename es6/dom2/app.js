dbf59501f684c7e96d081d9bbc76e8cd9bdbac59
Sujith kumar <k.sujithkumar@outlook.com>
2021-04-13 20:04:56 +0530

    loan calculator completed

--- es6/dom2/app.js
+++ es6/dom2/app.js
@@ -1,9 +1,18 @@
-document.getElementById("loan-form").addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e){
+    //show loader //Hide results
+    document.getElementById("results").style.display ="none";
+    document.getElementById("loading").style.display="block";
+    setTimeout(calculateResults, 2000);
 
-function calculateResults(e){
+    e.preventDefault();
+});
+
+function calculateResults(){
     console.log("Calculating....");
     calculate();
-    e.preventDefault();
+
+
+    
 }
 
 function calculate(){
@@ -23,6 +32,9 @@ function calculate(){
         mPayment.value = monthly.toFixed(2);
         totalPayment.value= (monthly*calculatedPayments).toFixed(2);
         totalInterest.value=((monthly* calculatedPayments)-principal).toFixed(2);
+        document.getElementById("results").style.display ="block";
+        document.getElementById("loading").style.display="none";
+      
     } else {
         showError("Please check your numbers");
     }
@@ -30,6 +42,8 @@ function calculate(){
 }
 
 function showError(error){
+    document.getElementById("results").style.display ="none";
+    document.getElementById("loading").style.display="none";
     const errDiv = document.createElement("div");
     errDiv.className ="alert alert-danger";
     const card=document.querySelector(".card");
